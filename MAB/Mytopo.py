add9e3670d75a2e20b0e3b5fb65d6e3b78266688
James Ren <james_rxy@hotmail.com>
2021-10-20 17:07:55 -0700

    update

---
+++ MAB/Mytopo.py
@@ -0,0 +1,87 @@
+import os
+import time
+from mininet.net import Mininet
+# from mininet.node import Controller, OVSKernelSwitch, OVSKernelAP
+from mininet.node import Node, Host, Switch
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+import sys
+
+def linkUpdate(n1, n2, link, totalTime, currentTime, currentLoss, lossStep, serverIp, port, targetLoss, numPacket):
+    i = 0
+    info("*** Update Link Param\n")
+    while True:
+        # n2.cmd('python3 sender.py %s %s %s %s', (serverIp, port, targetLoss, numPacket))
+        if currentTime > totalTime:
+            break;
+        if time.time() - currentTime > i+5:
+            info("\n*** Link Update")
+            link.delete()
+            link.__init__(n1, n2, cls = TCLink, bw = 100, delay = 10, loss = currentLoss+i*lossStep)
+            i += 5
+
+def topology():
+    info("Create a network.")
+    net = Mininet(link=TCLink)
+    global gnet
+    gnet = net
+
+    clientIp = '10.0.0.1/8'
+    serverIp = '10.0.0.2/8'
+    port = 8888
+    targetLoss = 5
+    numPacket = 10
+
+    info("*** Setting parameter\n")
+    totalTime = time.time() + 100  # total time = 100 seconds
+    currentTime = time.time()
+    currentLoss = 0
+    lossStep = 2
+
+
+    info("*** Creating nodes\n")
+    leftHost = net.addHost('c1', cls = Host, ip = clientIp)
+    rightHost = net.addHost('s1', cls = Host, ip = serverIp)
+    switch = net.addSwitch('sw1', cls = Switch)
+
+    info("*** Creating link\n")
+    l1 = net.addLink(leftHost, switch, cls=TCLink, delay = 3, loss = 5)
+    l2 = net.addLink(rightHost, switch, cls=TCLink, delay = 3, loss = 5)
+
+    info("*** Starting network\n")
+    net.build()
+
+    info("*** Enable Client-Socket model\n")  
+    rightHost.cmd('python3 receiver.py')
+    print("*** Receiver started ...")
+
+    linkUpdate(leftHost, rightHost, l1, totalTime, currentTime, currentLoss, lossStep, serverIp, port, targetLoss, numPacket)	
+   
+    # kills all the xterms that have been opened
+    # os.system('pkill xterm')
+
+    info ("*** Running CLI\n")
+    CLI(net)
+
+    info ("*** Stopping network\n")
+    net.stop()
+
+topos = { 'mytopo': ( lambda: topology() ) }    
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    try:
+        topology()
+    except:
+        type = sys.exc_info()[0]
+        error = sys.exc_info()[1]
+        traceback = sys.exc_info()[2]
+        print ("Type: %s" % type)
+        print ("Error: %s" % error)
+        print ("Traceback: %s" % traceback)
+        if gnet != None:
+            gnet.stop()
+        else:
+            print("No network was created...")
---
+++ MAB/finalTopol.py
@@ -0,0 +1,75 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   client --- switch --- server
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+import os
+import time
+from mininet.net import Mininet
+# from mininet.node import Controller, OVSKernelSwitch, OVSKernelAP
+from mininet.node import Node, Host, Switch
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+import sys
+from mininet.topo import Topo
+
+# def changeLoss():
+#   for intf in node.intfList(): # loop on interfaces of node
+#     #info( ' %s:'%intf )
+#     if intf.link: # get link that connects to interface(if any)
+#         newBW = 5
+#         intfs = [ intf.link.intf1, intf.link.intf2 ] #intfs[0] is source of link and intfs[1] is dst of link
+#         intfs[0].config(bw=newBW) 
+#         intfs[1].config(bw=newBW)
+#     else:
+#         info( ' \n' )
+
+# def linkUpdate(link, totalTime, currentTime, n1, n2, currentLoss, lossStep):
+#     i = 0
+#     info("*** Update Link Param\n")
+#     while True:
+#         if currentTime > totalTime:
+#             break;
+#         if time.time() - currentTime > i+5:
+#             link.delLink()
+#             link.addLink(n1, n2, cls = TCLink, bw = 100, delay = 10, loss = currentLoss+i*lossStep)
+#             i += 5
+
+
+class MyTopo( Topo ):
+    print("Simple topology example.")
+
+    def __init__( self ):
+        print("Create custom topo.")
+        totalTime = time.time() + 100  # total time = 100 seconds
+        currentTime = time.time()
+        currentLoss = 0
+        lossStep = 2
+
+        # Initialize topology
+        Topo.__init__( self)
+
+        # Add hosts and switches
+        info( "*** Creating nodes\n" )
+        leftHost = self.addHost( 'c1' , ip = '10.0.0.1/8')
+        rightHost = self.addHost( 's1', ip = '10.0.0.2/8' )
+        leftSwitch = self.addSwitch( 'sw1' )
+
+        info( "*** Creating links\n" )  
+        l1 = self.addLink( leftHost, leftSwitch, cls = TCLink, bw = 100, delay = 10, loss = currentLoss)
+        l2 = self.addLink( rightHost,leftSwitch, cls = TCLink, bw = 100, delay = 10, loss = currentLoss)
+
+        # rightHost.cmd('python3 receiver.py')
+        # leftHost.cmd('python3 sender.py 10.0.0.2 8888 5 100')
+
+        # linkUpdate(l1, totalTime, currentTime, leftHost, leftSwitch, currentLoss, lossStep)	
+        
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
+
